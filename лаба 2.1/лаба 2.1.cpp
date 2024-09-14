a1ffc3ca05eaf51c330fe1ec2d41a49a5a722bb9
vyshnevevarennya <natashaayebabki@gmail.com>
2024-09-14 20:36:23 +0300

    Lab2.1

--- лаба 2.1/лаба 2.1.cpp
+++ лаба 2.1/лаба 2.1.cpp
@@ -12,13 +12,13 @@ int main()
 	double Pi = 4 * atan(1.); // число пі
 	double alpha; // вхідний параметр
 	double z1; // результат обчислення 1-го виразу
-	double z2; // результат обчислення 2-го виразу
+	//double z2; // результат обчислення 2-го виразу
 	cout << "alpha = "; cin >> alpha;
 	z1 = 2*pow((sin(3* Pi - 2*alpha)), 2)*pow((cos(5* Pi +2*alpha)), 2);
-	z2 = 1.0/4-1.0/4*sin(5.0/2* Pi  - 8*alpha);
+	//z2 = 1.0/4-1.0/4*sin(5.0/2* Pi  - 8*alpha);
 	cout << endl;
 	cout << "z1 = " << z1 << endl;
-	cout << "z2 = " << z2 << endl; 
+	//cout << "z2 = " << z2 << endl; 
 cin.get();
 
 return 0;
